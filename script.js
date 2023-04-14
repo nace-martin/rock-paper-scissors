f7214b829a464cdf1a7235c6b70713fc80c647a7
nace <nason.s.martin@gmail.com>
2023-04-14 19:19:53 +1000

    Add Pseudocode.

---
+++ script.js
@@ -0,0 +1,5 @@
+// Define game rules and varibles
+// Prompt player to choose their move
+// Next determine the computers move
+// Next determine the outcome of the game ie winner/loser/draw
+// Lastly ask the player if he wants to play again.
