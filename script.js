55e800194b126e179f40aad9766144972a40f6e6
nace <nason.s.martin@gmail.com>
2023-04-14 20:35:21 +1000

    Add functon that randomly generates the computers move

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-// Define game rules and varibles
-// Prompt player to choose their move
-// Next determine the computers move
-// Next determine the outcome of the game ie winner/loser/draw
-// Lastly ask the player if he wants to play again.
+// Step 1. Create array for the options
+
+let options = ["Rock", "Paper", "Scissors"];
+
+// Step 2. Create a function that randomly generates the computers choice
+
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * options.length);
+  return options[random];
+}
+console.log(getComputerChoice);
+
+// Step 3. Create a function that prompts the user to choose an option from the array
+// Step 4. Next determine the outcome of the game ie winner/loser/draw
+// Step 5. Create a function that plays a single round and determines if the player won/lost/drawn
+//          and output a string like "You lost: Paper beats Rock"
+// Step 6. Create a loop that plays the game for 3 rounds and function that keeps
+//          score of both players
