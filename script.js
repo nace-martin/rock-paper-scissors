dd1cc72b9a1fa0dad3d54407e0f090f6baae6314
nace <nason.s.martin@gmail.com>
2023-04-26 04:53:36 +1000

    Add conditional if statement to determine a winner

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ function getComputerChoice() {
 // This Function plays a single round
 
 function playRound() {
-  let playerSelection = prompt("Rock", "Paper", "Scissors");
-  let getComputerChoice = computerSelection;
+  let playerSelection = prompt("Choice Rock, Paper or Scissors");
+
+  if (!["Rock", "Paper", "Scissors"].includes(playerSelection)) {
+    alert("Invalid Option. Please enter either Rock, Paper, or Scissors");
+    return;
+  }
+
+  let computerSelection = getComputerChoice();
+  if (playerSelection === computerSelection) {
+    alert(`It\'s a Tie you both choose ${playerSelection}`);
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
+  ) {
+    alert(`You Win ${playerSelection} beats ${computerSelection}`);
+  } else {
+    alert(`You Loose ${computerSelection} beats ${playerSelection}`);
+  }
 }
+
+playRound();
