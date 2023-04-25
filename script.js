55fc188d70e3ea520996aff146627a004af35a49
nace <nason.s.martin@gmail.com>
2023-04-26 04:17:41 +1000

    Validate players selection

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function getComputerChoice() {
   const options = ["Rock", "Paper", "Scissors"];
   return options[Math.floor(Math.random() * options.length)];
 }
+
+// This Function plays a single round
+
+function playRound() {
+  let playerSelection = prompt("Rock", "Paper", "Scissors");
+  let getComputerChoice = computerSelection;
+}
