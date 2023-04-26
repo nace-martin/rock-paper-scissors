d9cb85855edd44e30fffcab8600472c96fdcc264
nace <nason.s.martin@gmail.com>
2023-04-26 19:39:11 +1000

    Fix bug
    SyntaxError: Identifier 'playerSelection' has already been declared

--- script.js
+++ script.js
@@ -8,14 +8,6 @@ function getComputerChoice() {
 // This Function plays a single round
 
 function playRound(playerSelection, computerSelection) {
-  let playerSelection = prompt("Choice Rock, Paper or Scissors");
-
-  if (!["Rock", "Paper", "Scissors"].includes(playerSelection)) {
-    alert("Invalid Option. Please enter either Rock, Paper, or Scissors");
-    return;
-  }
-
-  let computerSelection = getComputerChoice();
   if (playerSelection === computerSelection) {
     return `It\'s a Tie you both choose ${playerSelection}`;
   } else if (
@@ -33,5 +25,11 @@ function game() {
   let playerScore = 0;
   let computerScore = 0;
 
-  for (let i = 0; i < 5; i++) {}
+  for (let i = 1; i <= 5; i++) {
+    let playerSelection = prompt("Choice Rock, Paper or Scissors");
+    if (!["Rock", "Paper", "Scissors"].includes(playerSelection)) {
+      alert("Invalid Option. Please enter either Rock, Paper, or Scissors");
+      return;
+    }
+  }
 }
